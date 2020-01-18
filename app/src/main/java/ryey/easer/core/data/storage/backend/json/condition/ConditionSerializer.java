a44ecca93247398c29b47c250c8725ebfa9e7e59
emaiannone <emaiannone96@gmail.com>
2020-01-18 12:11:41 +0100

    Refactor Member Ignoring Method smell in ConditionSerializer class

--- app/src/main/java/ryey/easer/core/data/storage/backend/json/condition/ConditionSerializer.java
+++ app/src/main/java/ryey/easer/core/data/storage/backend/json/condition/ConditionSerializer.java
@@ -44,10 +44,10 @@ public class ConditionSerializer implements Serializer<ConditionStructure> {
         }
     }
 
-    private JSONObject serialize_condition(ConditionData condition) throws JSONException {
-        JSONObject json_situation = new JSONObject();
-        json_situation.put(C.SPEC, LocalSkillRegistry.getInstance().condition().findSkill(condition).id());
-        json_situation.put(C.DATA, condition.serialize(PluginDataFormat.JSON));
-        return json_situation;
-    }
+    private static JSONObject serialize_condition(ConditionData condition) throws JSONException {
+		JSONObject json_situation = new JSONObject();
+		json_situation.put(C.SPEC, LocalSkillRegistry.getInstance().condition().findSkill(condition).id());
+		json_situation.put(C.DATA, condition.serialize(PluginDataFormat.JSON));
+		return json_situation;
+	}
 }
